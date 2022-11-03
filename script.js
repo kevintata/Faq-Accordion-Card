eed6edb1be837347cafc8f3c71c04917448a2472
Kevintata22 <kevintata22@gmail.com>
2022-11-03 20:45:34 -0300

    Made it perfect!

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-let toggles = document.getElementsByClassName('articlediv');
+let button = document.getElementsByClassName('articlediv');
 let buttontxt = document.getElementsByClassName('button')
-let contentDiv = document.getElementsByClassName('answer');
-let icons = document.getElementsByClassName('arrow');
+let answer = document.getElementsByClassName('answer');
+let arrow = document.getElementsByClassName('arrow');
 let border = document.querySelector('article')
 
-for(let i=0; i<toggles.length; i++){
-    toggles[i].addEventListener('click', ()=>{
-        if( parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight){
-            contentDiv[i].style.height = '56px'
+for(let i=0; i<button.length; i++){
+    button[i].addEventListener('click', ()=>{
+        if( parseInt(answer[i].style.height) != answer[i].scrollHeight){
+            answer[i].style.height = '56px'
             buttontxt[i].style.color = '#1d1e35'
             buttontxt[i].style.fontWeight = '900'
-            icons[i].style.transform = 'rotate(180deg)'
-            contentDiv[i].style.position = 'relative'
-            contentDiv[i].style.top = '-15px'
+            arrow[i].style.transform = 'rotate(180deg)'
+            answer[i].style.position = 'relative'
+            answer[i].style.top = '-15px'
         }
         else{
-            contentDiv[i].style.height = "0px";
-            icons[i].style.transform = 'rotate(0deg)'
+            answer[i].style.height = "0px";
+            arrow[i].style.transform = 'rotate(0deg)'
             buttontxt[i].style.color = '#787887'
             buttontxt[i].style.fontWeight = '300'
         }
 
-        for(let j=0; j<contentDiv.length; j++){
+        for(let j=0; j<answer.length; j++){
             if(j!==i){
-                contentDiv[j].style.height = "0px";
-                contentDiv[j].style.height = "0px";
-                icons[j].style.transform = 'rotate(0deg)'
+                answer[j].style.height = "0px";
+                answer[j].style.height = "0px";
+                arrow[j].style.transform = 'rotate(0deg)'
                 buttontxt[j].style.color = '#787887'
                 buttontxt[j].style.fontWeight = '300'
             }
